3b546c754ceb7be7ea2f393d57af00c626c46793
Ssharma0355 <ssharma0355@gmail.com>
2026-08-08 15:56:52 +0530

    using Hugging face api for chatmodel

---
+++ ChatModels/_chatmodel_hf_api.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-120b",
+    task="text-generation",
+    max_new_tokens=100,
+)
+
+model = ChatHuggingFace(llm=llm)
+
+response = model.invoke(
+    "Capital of India?"
+)
+
+print(response.content)
